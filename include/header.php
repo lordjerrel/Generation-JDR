a137a819d5a41fa63bf8fbe6f4307c5b6793bf0b
Mathieu B <95848649+lordjerrel@users.noreply.github.com>
2024-08-01 09:49:38 +0200

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,39 @@
+<?php session_start(); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-Transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+<head>
+<meta charset="utf-8"/>
+<title>Génération de trésors, objets magiques, auberges, scénari,...</title>
+<meta name="description" content="Site permettant de générer des trésors, de générer des objes magiques relatif à Donjon et Dragon 3.5 et applicable à tout univers médiéval fantastique"/>
+<meta name="title" content="Génération de trésors, objets magiques, auberges, scénari,... "/>
+
+<meta name="google-site-verification" content="Kp0IJqiQ9wNq_Q3WUEiyoIxT4YmwA-wCm8Fe712f_qA" />
+<link rel="stylesheet" type="text/css" href="design/style.css" media="screen" />
+<link rel="icon" type="image/png" href="/images/coffre.png" />
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-31379584-1']);
+  _gaq.push(['_setDomainName', 'generation-jdr.fr']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</head>
+<body>
+
+<div id="wrap">
+
+<div id="header">
+<h1>Générateur pour rôlistes</h1>
+<h2>En deux clics, générez beaucoup</h2>
+</div>
+
+<div id="right">
+<div class="articles">
